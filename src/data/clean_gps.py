08bf4452f18e08d3738da2f22a022e3a8d8ca0c8
Garrett Gibo <garrett.gibo@gmail.com>
2020-11-25 20:49:26 -0800

    Reduce clean_gps for methodology

--- src/data/clean_gps.py
+++ src/data/clean_gps.py
@@ -2,27 +2,22 @@
 cleans in batches and moves cleaned data to gps_data assuming our the path to
 our raw data looks something like this: '../data/raw/first_fix/'
 """
-import glob
-import os
 import pandas as pd
 
 
-def clean_gps(path):
+def clean_gps(**config) -> None:
     """Transfrom raw gps data into cleaned latitude and longitude csv"""
-    # make a new folder inside temp to hold cleaned data
-    # for each batch
-    new_folder = path.split("../data/raw/", 1)[1]
-    os.mkdir("../data/gps_data/"+new_folder)
-    all_raw_files = glob.glob(path + "/*.log")
+    df_gps = pd.read_csv(
+        config["test_path"],
+        index_col=None,
+        header=None,
+        names=['lat', 'lon', 'alt'])
+    df_gps.lat = df_gps.lat.str.replace("lat=", "").astype(float)
+    df_gps.lon = df_gps.lon.str.replace("lon=", "").astype(float)
+    df_gps.alt = df_gps.alt.str.replace("alt=", "").astype(float)
 
-    for file in all_raw_files:
-        print("Cleaning raw data at", file)
-
-        df_gps = pd.read_csv(file, index_col=None, header=None, names=['lat', 'lon', 'alt'])
-        df_gps.lat = df_gps.lat.str.replace("lat=", "").astype(float)
-        df_gps.lon = df_gps.lon.str.replace("lon=", "").astype(float)
-        df_gps.alt = df_gps.alt.str.replace("alt=", "").astype(float)
-
-        cleaned_file_path = file.replace(".log", "_cleaned.csv").replace("../data/raw", "../data/gps_data")
-        df_gps.to_csv(cleaned_file_path, index=False)
-    print("Cleaned csv for all raw data inside", new_folder, " at ", "../data/gps_data/"+new_folder)
+    cleaned_file_path = (
+        config["test_path"]
+        .replace(".log", "_cleaned.csv")
+    )
+    df_gps.to_csv(cleaned_file_path, index=False)
