cd243a87d77736211830ac687650df4331484ec0
ssliang <43868758+ssliang@users.noreply.github.com>
2020-11-26 01:49:01 -0500

    Update visualize.py

--- src/analysis/visualize.py
+++ src/analysis/visualize.py
@@ -1,49 +1,49 @@
-from gmplot import GoogleMapPlotter
-import pandas as pd
+# from gmplot import GoogleMapPlotter
+# import pandas as pd
 
 
-# We subclass this just to change the map type
-# taken from: https://stackoverflow.com/a/54164812
-class CustomGoogleMapPlotter(GoogleMapPlotter):
-    def __init__(self, center_lat, center_lng, zoom, apikey='',
-                 map_type='satellite'):
-        super().__init__(center_lat, center_lng, zoom, apikey)
+# # We subclass this just to change the map type
+# # taken from: https://stackoverflow.com/a/54164812
+# class CustomGoogleMapPlotter(GoogleMapPlotter):
+#     def __init__(self, center_lat, center_lng, zoom, apikey='',
+#                  map_type='satellite'):
+#         super().__init__(center_lat, center_lng, zoom, apikey)
 
-        self.map_type = map_type
-        assert(self.map_type in ['roadmap', 'satellite', 'hybrid', 'terrain'])
+#         self.map_type = map_type
+#         assert(self.map_type in ['roadmap', 'satellite', 'hybrid', 'terrain'])
 
-    def write_map(self,  f):
-        f.write('\t\tvar centerlatlng = new google.maps.LatLng(%f, %f);\n' %
-                (self.center[0], self.center[1]))
-        f.write('\t\tvar myOptions = {\n')
-        f.write('\t\t\tzoom: %d,\n' % (self.zoom))
-        f.write('\t\t\tcenter: centerlatlng,\n')
+#     def write_map(self,  f):
+#         f.write('\t\tvar centerlatlng = new google.maps.LatLng(%f, %f);\n' %
+#                 (self.center[0], self.center[1]))
+#         f.write('\t\tvar myOptions = {\n')
+#         f.write('\t\t\tzoom: %d,\n' % (self.zoom))
+#         f.write('\t\t\tcenter: centerlatlng,\n')
 
-        # This is the only line we change
-        f.write('\t\t\tmapTypeId: \'{}\'\n'.format(self.map_type))
-
-
-        f.write('\t\t};\n')
-        f.write(
-            '\t\tvar map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);\n')
-        f.write('\n')
+#         # This is the only line we change
+#         f.write('\t\t\tmapTypeId: \'{}\'\n'.format(self.map_type))
 
 
-def visualize(**config) -> None:
-    """Create google maps render of path that is reported from gps"""
-    initial_zoom = 16
+#         f.write('\t\t};\n')
+#         f.write(
+#             '\t\tvar map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);\n')
+#         f.write('\n')
 
-    df_gps = pd.read_csv(config["gps_data"])
 
-    lats = df_gps.lat
-    lons = df_gps.lon
+# def visualize(**config) -> None:
+#     """Create google maps render of path that is reported from gps"""
+#     initial_zoom = 16
 
-    gmap = CustomGoogleMapPlotter(
-            lats[0],
-            lons[0],
-            initial_zoom,
-            map_type='satellite')
+#     df_gps = pd.read_csv(config["gps_data"])
 
-    gmap.plot(lats, lons, 'cornflowerblue', edge_width=1)
+#     lats = df_gps.lat
+#     lons = df_gps.lon
 
-    gmap.draw(config["vis_path"])
+#     gmap = CustomGoogleMapPlotter(
+#             lats[0],
+#             lons[0],
+#             initial_zoom,
+#             map_type='satellite')
+
+#     gmap.plot(lats, lons, 'cornflowerblue', edge_width=1)
+
+#     gmap.draw(config["vis_path"])
