b508b2b5356b1876f3297737673e01cdef4bab70
Garrett Gibo <garrett.gibo@gmail.com>
2020-12-11 05:24:14 -0800

    Include folder log combination into gps cleaning

--- src/data/clean_gps.py
+++ src/data/clean_gps.py
@@ -12,6 +12,9 @@ def clean_gps(**config) -> None:
     """
     all_raw_folders = glob.glob(os.path.join(config["gps_raw_data"], '*'))
     for folder_path in all_raw_folders:
+        if "/0ft" in folder_path:
+            print("-" * 80)
+            clean_combine_fixed(folder_path)
         # create gps_data folder if it doesn't  exist
         folder_out = folder_path.replace("raw", "gps_data")
         if not os.path.isdir(folder_out):
@@ -36,24 +39,27 @@ def clean_gps(**config) -> None:
         print("Cleaned csv for all raw data inside", folder_path, "at",  folder_out)
 
 
-# Cleans every file within the fixed folder and combines
-# all of the cleaned files into one csv file
-def clean_combine_fixed() -> None:
-    
-    path = "../../data/raw/fixed"
-
-    # make a new folder inside temp to hold cleaned data 
-    # for each batch
-    all_raw_files = glob.glob(path + "/*.log")
+def clean_combine_fixed(folder_path) -> None:
+    """Cleans every file within the fixed folder and combines all of the cleaned 
+    files into one csv file.
+    """
+    print("Combining gps data")
+    # make a new folder inside temp to hold cleaned data for each batch
+    all_raw_files = glob.glob(folder_path + "/*.log")
     all_df = df = pd.DataFrame(columns = ['lat', 'lon', 'alt'])
     
-    for file in all_raw_files:
-        print("Cleaning raw data at", file)
-        df_x = pd.read_csv(file, index_col=None, header=None, names=['lat', 'lon', 'alt'])
+    # clean files inside batch and send to folder under same batch name in gps_dats
+    for log_file in all_raw_files:
+        print("Combining raw data at", log_file)
+        df_x = pd.read_csv(log_file, index_col=None, header=None, names=['lat', 'lon', 'alt'])
         df_x.lat = df_x.lat.str.replace("lat=", "").astype(float)
         df_x.lon = df_x.lon.str.replace("lon=", "").astype(float)
         df_x.alt = df_x.alt.str.replace("alt=", "").astype(float)
         all_df = all_df.append(df_x, ignore_index=True)
         
-    # is this relative path okay?
-    all_df.to_csv("../../data/fixed_all.csv", index=False)
+    folder_out = "data/CEP/"
+    if not os.path.isdir(folder_out):
+        os.makedirs(folder_out, exist_ok=True)
+
+    all_df_path = folder_out + "fixed_all.csv"
+    all_df.to_csv(all_df_path, index=False)
