cfb5aebf8f84fbdc245e1631872dc6a71ef64444
ssliang <43868758+ssliang@users.noreply.github.com>
2020-12-11 18:26:13 -0500

    Update model.py
    added error() and calc_distances_run(). Fixed format of comments

--- src/model/model.py
+++ src/model/model.py
@@ -7,14 +7,13 @@ import numpy as np
 from sklearn.metrics import mean_squared_error
 from math import sqrt
 
-# Calculates distance(in feet) between two pairs of coordinates
-# Note: Assumes gps outputs coordinates in degree not radian. 
-# Distance formula based on Vincenty's alg and assuming earth 
-# is spherical.
-def distance(lat1, lon1, lat2, lon2) -> float:
+def distance(lat1, lon1, lat2, lon2) -> Float:
+    """Calculates distance in feet between two pairs 
+    of coordinates using Vicenty's algorithm and 
+    assuming Earth is spherical. 
+    """
 
     #convert latitiude and longitude to spherical coords
-    #comment out if coords are already in radians
     lat1, lon1 = np.deg2rad(lat1), np.deg2rad(lon1)
     lat2, lon2 = np.deg2rad(lat2), np.deg2rad(lon2)
     
@@ -27,10 +26,11 @@ def distance(lat1, lon1, lat2, lon2) -> float:
     
     return r * np.arccos(a)
 
-# calculates root mean square error between a list of predictions 
-# and list of actual expected values
-def rmse(expected, actual) -> float:    
-    
+def rmse(expected, actual) -> Float:    
+    """ Calculates root mean squared error between the
+    calcualted distances of GPS outputted coordinates
+    and distance of track for each batch.
+    """
     total_preds = len(expected)
     sum_error = 0.0
     
@@ -40,12 +40,21 @@ def rmse(expected, actual) -> float:
         
     return sqrt(sum_error/float(total_preds))
 
-# Takes in a path like: "../../data/gps_data/first_fix"
-# Finds dist between start and end coordinates (first and last 
-# entry in each cleaned csv respectively). Compiles a df with 
-# batch name, run name, start coord, end coord, ground truth, 
-# calc'ed dist, and rmse for the whole batch rather than single run
-def calc_distances(path) -> pd.DataFrame():
+def error(expected, actual) -> Float:
+    """ Calculates error between expected distance
+    and actual distance as indicated by GPS outputs
+    for a single run.
+    """
+    total_preds = 1
+    squared_error = (actual[0]-expected[0])**2
+    
+    return sqrt(squared_error)
+
+def calc_distances_batch(path) -> pd.DataFrame():
+    """Compiles a dataframe with batch name, run name, start
+    position coordinates, end positionn coordinates, length 
+    of track, calculated distance, and rmse of the batch.
+    """
     
     ground_truth = int(path.split("../../data/gps_data/", 1)[1].replace("ft",""))
     files = glob.glob(path + "/*.csv")
@@ -63,7 +72,38 @@ def calc_distances(path) -> pd.DataFrame():
                                   'actual_dist' : calc_dist}, 
                                  ignore_index = True)
     df_dist.rmse = rmse(df_dist.expected_dist, df_dist.actual_dist)
+    
     return df_dist
+
+def calc_distances_run(path) -> pd.DataFrame():
+    """Compiles a dataframe with batch name, run name, start
+    position coordinates, end positionn coordinates, length 
+    of track, calculated distance, and rmse of each file.
+    """
+    
+    path = "../data/gps_data"
+    all_raw_folders = glob.glob(os.path.join(path, '*'))
+    df_all_runs = pd.DataFrame(columns = ['batch', 'run','start_lat_lon','finish_lat_lon','expected_dist','actual_dist','error'])
+
+
+    for folder in all_raw_folders:
+        ground_truth = int(folder.split("../data/gps_data/", 1)[1].replace("ft",""))
+        files = glob.glob(folder + "/*.csv")
+        for file in files:
+            df = pd.read_csv(file)
+            start = df.iloc[0]
+            finish = df.iloc[-1]
+            calc_dist = distance(start.lat, start.lon, finish.lat, finish.lon)
+            name = re.search('(?:[^/](?!\/))+(?=_cleaned.csv)', file)
+            batch = re.search('(?:\/gps_data\/)(.*(?=\/))', file)
+            row = pd.DataFrame({'batch': batch.group(1),'run' : name.group(), 'start_lat_lon': [(start.lat, start.lon)], 
+                                      'finish_lat_lon': [(finish.lat, finish.lon)], 'expected_dist' : ground_truth, 
+                                      'actual_dist' : calc_dist, 'error': "temp"}, index=[0])
+
+            row.error = error(row.expected_dist, row.actual_dist)
+            df_all_runs = df_all_runs.append(row)
+        
+    return df_all_runs
 
 def make_rmse_table(path):
     df = pd.DataFrame(columns = ['ground_truth', 'rmse'])
