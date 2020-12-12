b3fdc10f49e415231ba6411c92698441e8214126
Garrett Gibo <garrett.gibo@gmail.com>
2020-12-11 18:13:53 -0800

    Recompute CEP in UTM coordinate space

--- src/analysis/evaluate.py
+++ src/analysis/evaluate.py
@@ -1,34 +1,40 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import utm
+
 
 def cep(**config):
     """Calculate circular error probable of gps coordinates"""
     df_gps = pd.read_csv(config["full_fixed_gps"])
 
     lats, lons = df_gps.lat, df_gps.lon
-    cart_x, cart_y = df_gps.x, df_gps.y
+    utm_x, utm_y = convert_utm(list(zip(lats, lons)))
 
     # find delta "x" and "y"
-    lats -= np.mean(lats)
-    lons -= np.mean(lons)
-    cart_x -= np.mean(cart_x)
-    cart_y -= np.mean(cart_y)
+    utm_x -= np.mean(utm_x)
+    utm_y -= np.mean(utm_y)
 
-    sigma_lat, sigma_lon = np.std(lats), np.std(lons)
-    sigma_lat_c, sigma_lon_c = np.std(cart_x), np.std(cart_y)
+    sigma_x, sigma_y = np.std(utm_x), np.std(utm_y)
 
-    # cep equation
-    cep = 0.59 * (sigma_lat + sigma_lon)
-    cep_c = 0.59 * (sigma_lat_c + sigma_lon_c)
+    cep = 0.59 * (sigma_x + sigma_y)
+    drms_gps = two_d_rms(utm_x, utm_y)
 
-    drms_gps = two_d_rms(lats, lons)
-    drms_car = two_d_rms(cart_x, cart_y)
-
-    # plot_results(cep, drms_gps, lats, lons)
-    plot_results(cep_c, drms_car, df_gps.x, df_gps.y)
+    plot_results(cep, drms_gps, utm_x, utm_y)
 
     return cep
+
+
+def convert_utm(coords):
+    """Convert latitude and longitude into UTM coordinates"""
+    utm_x, utm_y = [], []
+    # convert every coordinate pair into utm
+    for lat, lon in coords:
+        utm_coord = utm.from_latlon(lat, lon)
+        utm_x.append(utm_coord[0])
+        utm_y.append(utm_coord[1])
+
+    return utm_x, utm_y
 
 
 def two_d_rms(x, y):
@@ -51,6 +57,8 @@ def plot_results(cep, drms, x, y):
     position = plt.scatter(x, y)
 
     plt.title("GPS Fixed Position")
+    plt.xlabel("delta x (m)")
+    plt.ylabel("delta y (m)")
     plt.legend([position, circle_cep, circle_drms],
                ["Distance From Average", "CEP (50%)", "2DRMS (95-98%)"])
 
@@ -59,5 +67,5 @@ def plot_results(cep, drms, x, y):
 
     plt.axis('equal')
 
-    # plt.show()
+    plt.show()
     
